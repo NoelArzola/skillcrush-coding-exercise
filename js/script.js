c408f407c4f517b0f79a0f5d9b7aea9034dc7180
Noel Arzola Jr <noel@noelarzola.com>
2021-06-29 04:23:56 -0400

    It works, removed debugging

--- js/script.js
+++ js/script.js
@@ -10,11 +10,6 @@ const toggleAllController = document.querySelector(
   '[data-toggle-role="control-all"]'
 );
 
-/* Grabs the true/false state of Expand All / Collapse all button */
-const toggleAllState = toggleAllController.getAttribute(
-  "data-main-toggle-active"
-);
-
 /* Grabs all of the elements that toggle and places them into an array */
 const collapsibles = Array.from(
   document.querySelectorAll('[data-toggle-role="collapsible"]')
@@ -38,29 +33,15 @@ const collapse = (selector, cmd) => {
   });
 };
 
-// const rotate = (a, b, c) => {
-//   if (a === "false") {
-//     targetCaret.classList.remove("-rotate-90");
-//     targetCaret.setAttribute("data-is-active", "true");
-//   } else if (e === "true") {
-//     elm.classList.add("-rotate-90");
-//     elm.setAttribute("data-is-active", "false");
-//   }
-// };
-
 /* Code block for the Expand/Collapse All button */
 const toggleAll = function () {
+  /* Grabs the true/false state of Expand All / Collapse all button */
+  const toggleAllState = toggleAllController.getAttribute(
+    "data-main-toggle-active"
+  );
+  /* This will: hide all collapsibles or show all, set states, as well as rotate the carets */
   const activateMainToggle = () => {
-    if (toggleAllState === "true") {
-      collapsibles.forEach((e) => {
-        e.classList.add("hidden");
-      });
-      carets.forEach((e) => {
-        e.classList.add("-rotate-90");
-        e.setAttribute("data-is-active", "false");
-      });
-      toggleAllController.setAttribute("data-main-toggle-active", "false");
-    } else if (toggleAllState === "false") {
+    if (toggleAllState === "false") {
       collapsibles.forEach((e) => {
         e.classList.remove("hidden");
       });
@@ -69,12 +50,23 @@ const toggleAll = function () {
         e.setAttribute("data-is-active", "true");
       });
       toggleAllController.setAttribute("data-main-toggle-active", "true");
+    } else {
+      collapsibles.forEach((e) => {
+        e.classList.add("hidden");
+      });
+      carets.forEach((e) => {
+        e.classList.add("-rotate-90");
+        e.setAttribute("data-is-active", "false");
+      });
+      toggleAllController.setAttribute("data-main-toggle-active", "false");
     }
   };
+  /* Swaps button text on click */
   const swapButtonText = () => {
     const icon = document.querySelector("[data-button-text='icon']");
     const expand = document.querySelector("[data-button-text='expand']");
     const collapse = document.querySelector("[data-button-text='collapse']");
+
     if (toggleAllState === "false") {
       expand.classList.add("hidden");
       collapse.classList.remove("hidden");
